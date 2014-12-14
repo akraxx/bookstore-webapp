4778046f224145a36906de39211bbd86023edbbe
maximilien marie <maximilien.marie@isen-lille.fr>
2014-12-14 18:59:27 +0100

    feat: update mailing address

--- app/scripts/controllers/profile.js
+++ app/scripts/controllers/profile.js
@@ -3,23 +3,38 @@
 angular.module('bookstoreWebapp')
     .controller('ProfileCtrl', function ($scope, $http, toaster) {
         $http.get('/api/user/me')
-            .success(function(me) {
-                $scope.profile = me;
-                $http.get('/api/mailingAddress/'+me.mailingAddressId).success(function(myAddress) {
-                    $scope.profile.address = myAddress;
-                });
-            });
+          .success(function(me) {
+            $scope.profile = me;
+            $scope.profile.address = {};
+            if(me.mailingAddressId) {
+              $http.get('/api/mailingAddress/'+me.mailingAddressId).success(function(myAddress) {
+                $scope.profile.address = myAddress;
+              });
+            }
+          });
 
         $scope.submit = function() {
             console.log($scope.profile);
             $http.put('/api/user/updateEmail', $scope.profile.email)
               .success(function() {
-                toaster.pop('success', 'Profile updated', 'Your profile has been updated.', 5000, 'trustedHtml');
-              })
-              .error(function(data, status) {
-                if(status === 304) {
-                  toaster.pop('warning', 'No changes', 'No changes have been found.', 5000, 'trustedHtml');
-                }
+                toaster.pop('success', 'Email has been updated', 'Your profile has been updated.', 5000, 'trustedHtml');
               });
+
+          $http.put('/api/mailingAddress', $scope.profile.address)
+            .success(function() {
+              toaster.pop('success', 'Address updated', 'Your address has been updated.', 5000, 'trustedHtml');
+            })
+            .error(function(data) {
+
+              if(data.errors) {
+                var errors = '<ul>';
+                angular.forEach(data.errors, function(value) {
+                  errors += '<li>' + value + '</li>';
+                }, errors);
+                errors += '</ul>';
+                toaster.pop('error', 'Error', 'Error(s) occurred during the registration : ' + errors, 0, 'trustedHtml');
+              }
+
+            });
         };
     });
