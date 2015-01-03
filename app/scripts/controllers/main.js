7f457ec3d2a3433ef8c6fafd132045a20e6883d6
maximilien marie <maximilien.marie@isen-lille.fr>
2015-01-03 18:05:58 +0100

    feat(main): change main page

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,23 @@
  */
 angular.module('bookstoreWebapp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+    $scope.technologiesFront = [
       'HTML5 Boilerplate',
       'AngularJS',
-      'Karma'
+      'Karma',
+      'CSS 3',
+      'Bootstrap',
+      'ng-table',
+      'toaster'
+    ];
+
+    $scope.technologiesBack = [
+      'Jersey',
+      'JPA',
+      'Hibernate',
+      'Jackson',
+      'Jetty',
+      'Guice',
+      'Dropwizard'
     ];
   });
